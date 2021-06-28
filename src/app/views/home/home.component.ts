9dc2baaa2645147ff625e58d0a3f9d65bd715bf1
Alexandre Almeida <alexandre.almeida@apptek.com.br>
2021-06-28 10:09:27 -0300

    edit item list

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -47,15 +47,32 @@ export class HomeComponent implements OnInit {
         name: '',
         weight: null,
         symbol: ''
-      } : element
+      } : {
+        position: element.position,
+        name: element.name,
+        weight: element.weight,
+        symbol: element.symbol
+      }
     });
 
     dialogRef.afterClosed().subscribe(result => {
       if(result !== undefined) {
-        this.dataSource.push(result);
+        if (this.dataSource.map(p => p.position).includes(result.position)) {
+          this.dataSource[result.position - 1] = result;
+        } else {
+          this.dataSource.push(result);
+        }
         this.table.renderRows();
       }
     });
+  }
+
+  editElement(element: PeriodicElement): void {
+    this.openDialog(element);
+  }
+
+  deleteElement(position: number): void {
+      this.dataSource = this.dataSource.filter(p => p.position !== position);
   }
 
 }
